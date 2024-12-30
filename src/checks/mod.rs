0c027f05acbefa0aa46e6f63327572dcef07c9a8
Gianmarco Fraccaroli <gianmarco.fraccaroli@gmail.com>
2024-12-30 16:41:13 +0100

    add inflation check

--- src/checks/mod.rs
+++ src/checks/mod.rs
@@ -1,18 +1,22 @@
 pub mod block;
 pub mod epoch;
+pub mod total_supply_native;
 
 use block::BlockHeigtCheck;
 use epoch::EpochCheck;
+use total_supply_native::TotalSupplyNativeCheck;
 
 
 pub enum Checks {
     BlockHeightCheck(BlockHeigtCheck),
     EpochCheck(EpochCheck),
+    TotalSupplyNative(TotalSupplyNativeCheck)
 }
 
 pub fn all_checks() -> Vec<Checks> {
     vec![
         Checks::BlockHeightCheck(BlockHeigtCheck::default()),
         Checks::EpochCheck(EpochCheck::default()),
+        Checks::TotalSupplyNative(TotalSupplyNativeCheck::default()),
     ]
 }
