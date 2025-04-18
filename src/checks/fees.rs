7edbd8da9de162949948d4bd212d75a74d6e4d07
Felipe Manzano <felipe.andres.manzano@gmail.com>
2025-04-18 12:20:37 -0300

    Micro thresholds

--- src/checks/fees.rs
+++ src/checks/fees.rs
@@ -1,6 +1,9 @@
 use super::{AppConfig, CheckTrait, State};
 use std::collections::HashMap;
 
+
+const MICRO:usize = 1000000;
+
 type Token = String;
 struct DefaultFeeThreshold {
     name: String,
@@ -77,7 +80,7 @@ impl FeeCheck {
                 "tnam1phks0geerggjk96ezhxclt6r5tdgu3usa5zteyyc".to_string(),
                 DefaultFeeThreshold {
                     name: "transfer/channel-0/uosmo".to_string(),
-                    value: 0.05,
+                    value: 0.05 * MICRO,
                 },
             );
             //https://github.com/Luminara-Hub/namada-ecosystem/blob/main/user-and-dev-tools/testnet/housefire/explorers.json
@@ -96,84 +99,84 @@ impl FeeCheck {
                 "tnam1phks0geerggjk96ezhxclt6r5tdgu3usa5zteyyc".to_string(),
                 DefaultFeeThreshold {
                     name: "transfer/channel-0/uosmo".to_string(),
-                    value: 0.0,
+                    value: 0.0 * MICRO,
                 },
             );
             self.thresholds.insert(
                 "tnam1phzvlar06m0rtjjv7n8qx8ny8j8aexayhyq98r7s".to_string(),
                 DefaultFeeThreshold {
                     name: "transfer/channel-1/uatom".to_string(),
-                    value: 0.0,
+                    value: 0.0 * MICRO,
                 },
             );
             self.thresholds.insert(
                 "tnam1phdf4sns3dx653kjfeejgymnehxg2z7xgs4z956n".to_string(),
                 DefaultFeeThreshold {
                     name: "transfer/channel-10/utia".to_string(),
-                    value: 0.02,
+                    value: 0.02 * MICRO,
                 },
             );
             self.thresholds.insert(
                 "tnam1pk22zc02efq85wvgnu6q3zfe07sz828p35xntldz".to_string(),
                 DefaultFeeThreshold {
                     name: "transfer/channel-4/stuatom".to_string(),
-                    value: 0.0,
+                    value: 0.0 * MICRO,
                 },
             );
             self.thresholds.insert(
                 "tnam1p4ak7rgnqatppd0hjnfsvu7dray8twf0sv2rvq3f".to_string(),
                 DefaultFeeThreshold {
                     name: "transfer/channel-4/stuosmo".to_string(),
-                    value: 0.0,
+                    value: 0.0 * MICRO,
                 },
             );
             self.thresholds.insert(
                 "tnam1ph4d4cdwu3tvj8rj6n75lrp3q0pg0yym7gpf75az".to_string(),
                 DefaultFeeThreshold {
                     name: "transfer/channel-4/stutia".to_string(),
-                    value: 0.0,
+                    value: 0.0 * MICRO,
                 },
             );
             self.thresholds.insert(
                 "tnam1p4r2835fw404zme26y88uxex8lnp5rdv4s9yjtu7".to_string(),
                 DefaultFeeThreshold {
                     name: "transfer/channel-5/utia".to_string(),
-                    value: 0.0,
+                    value: 0.0 * MICRO,
                 },
             );
             self.thresholds.insert(
                 "tnam1phavrw42dmxuhzzq3fhwagf663ekmf58lqedrqcv".to_string(),
                 DefaultFeeThreshold {
                     name: "transfer/channel-7/uosmo".to_string(),
-                    value: 0.02,
+                    value: 0.02 * MICRO,
                 },
             );
             self.thresholds.insert(
                 "tnam1pkmcvjcruxul6ncyjfp7j24ady2cda5zzvudakty".to_string(),
                 DefaultFeeThreshold {
                     name: "transfer/channel-8/stuatom".to_string(),
-                    value: 0.02,
+                    value: 0.02 * MICRO,
                 },
             );
             self.thresholds.insert(
                 "tnam1p46jfxscmma7le2lswcuwr9dydxlze83wsjdkygq".to_string(),
                 DefaultFeeThreshold {
                     name: "transfer/channel-8/stuosmo".to_string(),
-                    value: 0.02,
+                    value: 0.02 * MICRO,
                 },
             );
             self.thresholds.insert(
                 "tnam1p4jknczaacetxwwe9p49903nml80e9ex0ufqh3kr".to_string(),
                 DefaultFeeThreshold {
                     name: "transfer/channel-8/stutia".to_string(),
-                    value: 0.02,
+                    value: 0.02 * MICRO,
                 },
             );
             self.thresholds.insert(
                 "tnam1p4zuqqd94csj6zv8n0jxylz9kex4vdsgvg3uglw9".to_string(),
                 DefaultFeeThreshold {
                     name: "transfer/channel-9/uatom".to_string(),
-                    value: 0.02,
+                    value: 0.02 * MICRO,
                 },
             );
             self.explorer =
@@ -192,42 +195,42 @@ impl FeeCheck {
                 "tnam1p5z5538v3kdk3wdx7r2hpqm4uq9926dz3ughcp7n".to_string(),
                 DefaultFeeThreshold {
                     name: "transfer/channel-0/stuatom".to_string(),
-                    value: 0.05,
+                    value: 0.05 * MICRO,
                 },
             );
             self.thresholds.insert(
                 "tnam1p4px8sw3am4qvetj7eu77gftm4fz4hcw2ulpldc7".to_string(),
                 DefaultFeeThreshold {
                     name: "transfer/channel-0/stuosmo".to_string(),
-                    value: 0.5,
+                    value: 0.5 * MICRO,
                 },
             );
             self.thresholds.insert(
                 "tnam1ph6xhf0defk65hm7l5ursscwqdj8ehrcdv300u4g".to_string(),
                 DefaultFeeThreshold {
                     name: "transfer/channel-0/stutia".to_string(),
-                    value: 0.05,
+                    value: 0.05 * MICRO,
                 },
             );
             self.thresholds.insert(
                 "tnam1p5z8ruwyu7ha8urhq2l0dhpk2f5dv3ts7uyf2n75".to_string(),
                 DefaultFeeThreshold {
                     name: "transfer/channel-1/uosmo".to_string(),
-                    value: 0.5,
+                    value: 0.5 * MICRO,
                 },
             );
             self.thresholds.insert(
                 "tnam1pkg30gnt4q0zn7j00r6hms4ajrxn6f5ysyyl7w9m".to_string(),
                 DefaultFeeThreshold {
                     name: "transfer/channel-2/uatom".to_string(),
-                    value: 0.05,
+                    value: 0.05 * MICRO,
                 },
             );
             self.thresholds.insert(
                 "tnam1pklj3kwp0cpsdvv56584rsajty974527qsp8n0nm".to_string(),
                 DefaultFeeThreshold {
                     name: "transfer/channel-3/utia".to_string(),
-                    value: 0.05,
+                    value: 0.05 * MICRO,
                 },
             );
             self.explorer = "https://explorer75.org/namada/tx".to_string();
