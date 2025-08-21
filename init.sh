6413dff54146fc5c337c82e686b15f0d179954c5
Gianmarco Fraccaroli <gianmarco.fraccaroli@gmail.com>
2025-08-21 15:11:05 +0200

    improve init.sh

--- init.sh
+++ init.sh
@@ -1,11 +1,5 @@
 #!/bin/bash
 
-# Check if CONFIG_URL is set
-if [ -z "$CONFIG_URL" ]; then
-  echo "CONFIG_URL is not set."
-  exit 1
-fi
-
 # Check if CHAIN_ID is set
 if [ -z "$CHAIN_ID" ]; then
   echo "CHAIN_ID is not set."
@@ -18,10 +12,22 @@ if [ -z "$RPC" ]; then
   exit 1
 fi
 
-# Download the file
-curl -fSL "$CONFIG_URL" -o config.toml || {
-  echo "Download failed."
+# Check if CONFIG_PATH is set
+if [ -n "$CONFIG_PATH" ]; then
+  if [ ! -f "$CONFIG_PATH" ]; then
+    echo "CONFIG_PATH file does not exist."
+    exit 1
+  fi
+  CONFIG_FILE="$CONFIG_PATH"
+elif [ -n "$CONFIG_URL" ]; then
+  curl -fSL "$CONFIG_URL" -o config.toml || {
+    echo "Download failed."
+    exit 1
+  }
+  CONFIG_FILE="config.toml"
+else
+  echo "Neither CONFIG_PATH nor CONFIG_URL is set."
   exit 1
-}
+fi
 
-/app/monitoring --chain-id $CHAIN_ID --rpc $RPC --config-path config.toml
+/app/monitoring --chain-id $CHAIN_ID --rpc $RPC --config-path $CONFIG_FILE
